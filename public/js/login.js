620ee1e82e4f007db237c49914123a8ae3c9dd10
Josh A <jmalm79@gmail.com>
2021-04-25 20:57:15 -0500

    Updated pages, addes homepage info, created newchild and newfamily pages, added update and delete buttons

--- public/js/login.js
+++ public/js/login.js
@@ -1,16 +1,20 @@
 
 const loginForm = async (event) => {
     event.preventDefault();
+
     const email = document.querySelector('#email-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
     
     if (email && password) {
+
         const response = await fetch('/api/users/login', {
             method: 'POST',
             body: JSON.stringify({ email, password}),
             headers: {'Content-Type': 'application/json'},
         });
+
         if (response.ok) {
+          
             document.location.replace('/api/internal');
         } else {
             alert(response.statusText);
@@ -20,6 +24,7 @@ const loginForm = async (event) => {
 
 const signupForm = async (event) => {
     event.preventDefault();
+    
     const username = document.querySelector('#username-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
